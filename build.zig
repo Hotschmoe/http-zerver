404fb6c8e40a16646cdd0b4bd6865955bec7e5b3
hotschmoe <stronggarner66@gmail.com>
2025-02-25 13:52:50 -0700

    working
    but 404

--- build.zig
+++ build.zig
@@ -25,11 +25,40 @@ pub fn build(b: *std.Build) void {
     // Create a run step
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    // Add run step
-    const run_step = b.step("run", "Run the HTTP server");
-    run_step.dependOn(&run_cmd.step);
+    
+    // Create a custom run step that:
+    // 1. Deletes all files in /www
+    // 2. Copies /assets to /www
+    // 3. Copies the executable to /www
+    // 4. Runs the server from /www on port 8000
+    
+    // Create a custom run step
+    const custom_run_step = b.step("run", "Run the HTTP server from /www directory");
+    
+    // Add a command to clear the www directory
+    const clear_www = b.addSystemCommand(&[_][]const u8{
+        "powershell", "-Command", "if (Test-Path www) { Remove-Item -Path www\\* -Recurse -Force }; if (-not (Test-Path www)) { New-Item -ItemType Directory -Path www }"
+    });
+    
+    // Add a command to copy all files from /assets to /www
+    const copy_assets = b.addSystemCommand(&[_][]const u8{
+        "powershell", "-Command", "if (Test-Path assets) { Copy-Item -Path assets\\* -Destination www\\ -Recurse -Force }"
+    });
+    copy_assets.step.dependOn(&clear_www.step);
+    
+    // Add a command to copy the executable to /www
+    const copy_exe = b.addSystemCommand(&[_][]const u8{
+        "powershell", "-Command", "Copy-Item",
+        b.fmt("{s}/bin/http-zerver.exe", .{b.install_path}), "www/http-zerver.exe",
+    });
+    copy_exe.step.dependOn(b.getInstallStep());
+    copy_exe.step.dependOn(&copy_assets.step);
+    
+    // Add a command to run the server from /www
+    const run_server = b.addSystemCommand(&[_][]const u8{
+        "powershell", "-Command", "cd www; ./http-zerver.exe 9000 ."
+    });
+    run_server.step.dependOn(&copy_exe.step);
+    
+    custom_run_step.dependOn(&run_server.step);
 } 
